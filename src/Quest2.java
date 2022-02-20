3cd030bb8d0693b48b5b0361205ecb01a09b9018
dilima1 <95257477+dilima1@users.noreply.github.com>
2022-02-20 07:47:19 -0300

    Update Quest2.java

--- src/Quest2.java
+++ src/Quest2.java
@@ -36,15 +36,15 @@ class Quest2 {
     {
   
         // Test Case 1:
-        String str1 = "Geeks@portal20";
+        String str1 = "Edilson@test20";
         System.out.println(isValidPassword(str1));
   
         // Test Case 2:
-        String str2 = "Geeksforgeeks";
+        String str2 = "Edilsonlima";
         System.out.println(isValidPassword(str2));
   
         // Test Case 3:
-        String str3 = "Geeks@ portal9";
+        String str3 = "trix@ tes20";
         System.out.println(isValidPassword(str3));
   
         // Test Case 4:
@@ -53,11 +53,11 @@ class Quest2 {
   
         // Test Case 5:
 
-        String str5 = "Gfg@20";
+        String str5 = "Edi@20";
         System.out.println(isValidPassword(str5));
   
         // Test Case 6:
-        String str6 = "geeks@portal20";
+        String str6 = "trix@portal20";
         System.out.println(isValidPassword(str6));
     }
 }
